89d261ddc42f8d236a3b4b576075659b47d3bf1b
unknown <rakeshmangla00@gmail.com>
2020-02-07 00:16:55 +0530

    --

---
+++ src/test/java/com/yash/AssignmentCore/WrongInSeriesTest.java
@@ -0,0 +1,29 @@
+package com.yash.AssignmentCore;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class WrongInSeriesTest {
+
+	WrongInSeries wrongInSeries = new WrongInSeries();
+
+	@Test
+	public void shouldReturnWrongNumberInSeries() {
+		int expected = 19;
+
+		int actual = wrongInSeries.getWrongInSeries(Arrays.asList(3, 12, 8, 19, 13, 32, 18, 42, 23, 52));
+		assertEquals(expected, actual);
+	}
+
+	@Test
+	public void shouldReturnZeroWhenThereIsNoWrongNumberInSeries() {
+		int expected = 0;
+
+		int actual = wrongInSeries.getWrongInSeries(Arrays.asList(3, 12, 8, 22, 13, 32, 18, 42, 23, 52));
+		assertEquals(expected, actual);
+	}
+
+}
